e8d2d487e663148c2e0459b2cb1871fe2c3308a4
Alperen Öncel <alperenoncel@gmail.com>
2023-11-10 23:29:54 +0300

    new API's added and little fixes

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -3,7 +3,6 @@ using CustomerManagementSystem;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-[Authorize(Roles = "Admin")] // Sadece Admin yetkisi olanlar erişebilir.
 [ApiController]
 [Route("api/[controller]")]
 public class CustomerController : ControllerBase
@@ -15,6 +14,7 @@ public class CustomerController : ControllerBase
         _context = context;
     }
 
+    [Authorize(Roles = "Admin")] // Only Admins Can Access
     [HttpPost]
     public IActionResult AddCustomer(string name, string email)
     {
@@ -24,7 +24,7 @@ public class CustomerController : ControllerBase
             Email= email
         };
         _context.Customers.Add(customer);
-        _context.SaveChanges();
+        _context.SaveChangesAsync();
         return Ok("Müşteri başarıyla eklendi.");
     }
 
@@ -33,5 +33,29 @@ public class CustomerController : ControllerBase
     {
         var customers = _context.Customers.ToList();
         return Ok(customers);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetCustomerById(int id)
+    {
+        var customer = _context.Customers.FirstOrDefault(x => x.Id == id);
+        return Ok(customer);
+    }
+
+    [Authorize(Roles = "Admin")] // Only Admins Can Access
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCustomer(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
